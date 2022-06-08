f7eb5fe24c9a0576d78f924efb8727f4ad112bb7
Ayoub_L <ayoublaissaoui.1@gmail.com>
2022-06-08 16:55:00 +0100

    added gif search to client

--- client/JS/app.js
+++ client/JS/app.js
@@ -1,4 +1,36 @@
 
+const APIKEY = "EuG6ds89Ls0W1mXGMR4fxgxbkHLEW3HI";
+
+function CreateGifs(e){
+        e.preventDefault();
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+        let str = document.getElementById("search").value.trim();
+        url = url.concat(str);
+        console.log(url);
+        fetch(url)
+          .then(response => response.json())
+          .then(content => {
+            console.log(content.data);
+            console.log("META", content.meta);
+            let fig = document.createElement("figure");
+            let img = document.createElement("img");
+            let fc = document.createElement("figcaption");
+            img.src = content.data[0].images.downsized.url;
+            img.alt = content.data[0].title;
+            fc.textContent = content.data[0].title;
+            fig.appendChild(img);
+            fig.appendChild(fc);
+            let out = document.querySelector(".out");
+            out.insertAdjacentElement("afterbegin", fig);
+            document.querySelector("#search").value = "";
+          })
+          .catch(err => {
+            console.error(err);
+          });
+}
+
+//////////////////////////   POSTS AND COMMENTS SECTION  /////////////////////////////////////
+
 function getAllPosts(){
     fetch('http://localhost:5000/posts')
         .then(r => r.json())
@@ -152,5 +184,7 @@ module.exports = {
     submitComment,
     deletePost,
     deleteComm,
+    init,
+    CreateGifs,
     renderMessage
 }
